257f7947bcdf71737826ae1991b1512f22bc3335
Bernie Stern <bernzs@gmail.com>
2017-11-01 20:43:47 -0400

    switch to hapi

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@
 require('babel-register')({});
 require("babel-polyfill");
 
-var server = require('./server').default;
-const PORT = process.env.PORT || 3000;
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // only for development on localhost
 
-server.listen(PORT, function (err) {
-  if (err) {
-    throw new Error(err);
-  }
-
-  console.log('Server listening on port', PORT);
-});
+require('./server');
--- src/server.jsx
+++ src/server.jsx
@@ -1,10 +1,9 @@
 import http2 from 'http2';
-import http from 'http'; // TODO delete
 import { PassThrough } from 'stream';
 import React from 'react';
 import { renderToNodeStream } from 'react-dom/server'
 import * as fs from 'fs';
-import express from 'express';
+import Hapi from 'hapi';
 import buildFetch from 'fetch-push';
 
 import AppComponent from './components';
@@ -14,36 +13,59 @@ const options = {
   cert: fs.readFileSync('./server.crt'),
   allowHTTP1: true
 };
-const app = express();
 
-// const server = http2.createSecureServer(options, app);
-const server = http.createServer(app);
+const httpServer = http2.createSecureServer(options);
 
-app.get('/', function(req, res) {
-  const fetch = buildFetch(res);
-
-  const InitialComponent = (<AppComponent fetch={fetch} />);
-  // You could also build a Provider component to place fetch in react context,
-  // and use a high order component to retrieve fetch from the context.
-
-  const html1 = `<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Page Title</title>
-  </head>
-  <body>
-    <div id="react-view">`;
-  res.write(html1);
-  const stream = renderToNodeStream(InitialComponent);
-  stream.pipe(res, {end: false});
-  stream.on('end', () => {
-    const html2 = `</div>
-    <script defer async type="application/javascript" src="/assets/bundle.js"></script>
-  </body>
-</html>`;
-    res.end(html2);
-  });
+const server = new Hapi.Server({
+  debug: {
+    log: '*',
+    request: '*'
+  },
+  listener: httpServer,
+  tls: true,
+  port: 3000
 });
 
-export default server;
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, h) {
+    const { res } = request.raw; // This is where hapi puts the response object
+
+    const fetch = buildFetch(res);
+
+    const InitialComponent = (<AppComponent fetch={fetch} />);
+    // You could also build a Provider component to place fetch in react context,
+    // and use a high order component to retrieve fetch from the context.
+
+    const responseBody = new PassThrough();
+
+    const html1 = `<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Page Title</title>
+    </head>
+    <body>
+      <div id="react-view">`;
+    responseBody.write(html1);
+    const stream = renderToNodeStream(InitialComponent);
+    stream.pipe(responseBody, {end: false});
+    stream.on('end', () => {
+      const html2 = `</div>
+      <script defer async type="application/javascript" src="/assets/bundle.js"></script>
+    </body>
+  </html>`;
+      responseBody.end(html2);
+    });
+
+    return h.response(responseBody);
+  }
+});
+
+server.start(err => {
+    if (err) {
+        console.error('Server error', err);
+    }
+    console.log( `Server started at ${ server.info.uri }` );
+});
